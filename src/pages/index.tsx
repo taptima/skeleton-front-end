f761927b385849c6b2b41a4a422f1b04a808c385
Dmitriy Zvezdetskiy <zvezdetskiy@taptima.ru>
2022-10-18 15:55:49 +1000

    chore: remove example repository

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
 import createPage from 'presentation/pageBuilder/createPage';
 import HomePage from 'presentation/component/page/home';
-import Controller from 'presentation/component/page/home/Controller';
 
-export default createPage(HomePage, {
-    getInitialProps: async (container) => {
-        await container.get(Controller).initialAction();
-    },
-});
+export default createPage(HomePage);
